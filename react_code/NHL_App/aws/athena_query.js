0b80857cc359479b83a0a98f1a6831dc0e7c6ef6
Rudolph Dees <rudydees96@gmail.com>
2025-04-19 23:28:48 -0400

    We got data from aws into the app.
    Woop Woop

---
+++ react_code/NHL_App/aws/athena_query.js
@@ -0,0 +1,22 @@
+const query_athena = async (query) => {
+  try {
+    console.log("Querying Athena with query:", query);
+    const response = await fetch('https://878r18dvk4.execute-api.us-east-1.amazonaws.com/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        query: { query },
+      }),
+    });
+
+    const data = await response.json();
+    console.log("Athena results:", data);
+
+    return data.results; // Return the results properly
+  } catch (err) {
+    console.error("Error querying Athena:", err);
+    throw err; // Re-throw the error to handle it in the calling function
+  }
+};
+
+export default query_athena;
